c377f47700b891782e54b7e4cb897875c838b47a
代文韬 <dwt136@163.com>
2016-01-23 17:31:57 +0800

    test

--- spider.py
+++ spider.py
@@ -1,163 +1,191 @@
 #!/usr/bin/env python2
-from gevent import monkey; monkey.patch_socket()
 from pyquery import PyQuery as pq
-import gevent
-import requests
+import requesocks
 import json
+import logging
+import time
 
 
-entries = [
-    'http://www.meishij.net/chufang/diy/jiangchangcaipu/',
-    'http://www.meishij.net/chufang/diy/sijiacai/',
-    'http://www.meishij.net/chufang/diy/langcaipu/',
-    'http://www.meishij.net/chufang/diy/haixian/',
-    'http://www.meishij.net/chufang/diy/recaipu/',
-    'http://www.meishij.net/chufang/diy/tangbaocaipu/',
-    'http://www.meishij.net/chufang/diy/sushi/',
-    'http://www.meishij.net/chufang/diy/jiangliaozhanliao/',
-    'http://www.meishij.net/chufang/diy/weibolucaipu/',
-    'http://www.meishij.net/chufang/diy/huoguo/',
-    'http://www.meishij.net/chufang/diy/tianpindianxin/',
-    'http://www.meishij.net/chufang/diy/gaodianxiaochi/',
-    'http://www.meishij.net/chufang/diy/ganguo/',
-    'http://www.meishij.net/chufang/diy/rujiangcai/',
-    'http://www.meishij.net/chufang/diy/yinpin/',
-    'http://www.meishij.net/chufang/diy/zaocan/',
-    'http://www.meishij.net/chufang/diy/wucan/',
-    'http://www.meishij.net/chufang/diy/wancan/',
-    'http://www.meishij.net/chufang/diy/xiawucha/',
-    'http://www.meishij.net/chufang/diy/yexiao/',
-    'http://www.meishij.net/chufang/diy/laonian/',
-    'http://www.meishij.net/chufang/diy/chanfu/',
-    'http://www.meishij.net/chufang/diy/yunfu/',
-    'http://www.meishij.net/chufang/diy/baobaocaipu/',
-    'http://www.meishij.net/yaoshanshiliao/jibingtiaoli/',
-    'http://www.meishij.net/yaoshanshiliao/gongnengxing/',
-    'http://www.meishij.net/yaoshanshiliao/zangfu/',
-    'http://www.meishij.net/yaoshanshiliao/renqunshanshi/',
-    'http://www.meishij.net/china-food/caixi/chuancai/',
-    'http://www.meishij.net/china-food/caixi/xiangcai/',
-    'http://www.meishij.net/china-food/caixi/yuecai/',
-    'http://www.meishij.net/china-food/caixi/dongbeicai/',
-    'http://www.meishij.net/china-food/caixi/lucai/',
-    'http://www.meishij.net/china-food/caixi/zhecai/',
-    'http://www.meishij.net/china-food/caixi/sucai/',
-    'http://www.meishij.net/china-food/caixi/qingzhencai/',
-    'http://www.meishij.net/china-food/caixi/mincai/',
-    'http://www.meishij.net/china-food/caixi/hucai/',
-    'http://www.meishij.net/china-food/caixi/jingcai/',
-    'http://www.meishij.net/china-food/caixi/hubeicai/',
-    'http://www.meishij.net/china-food/caixi/huicai/',
-    'http://www.meishij.net/china-food/caixi/yucai/',
-    'http://www.meishij.net/china-food/caixi/xibeicai/',
-    'http://www.meishij.net/china-food/caixi/yuguicai/',
-    'http://www.meishij.net/china-food/caixi/jiangxicai/',
-    'http://www.meishij.net/china-food/caixi/shancicai/',
-    'http://www.meishij.net/china-food/caixi/guangxicai/',
-    'http://www.meishij.net/china-food/caixi/gangtai/',
-    'http://www.meishij.net/china-food/caixi/other/',
-    'http://www.meishij.net/china-food/xiaochi/sichuan/',
-    'http://www.meishij.net/china-food/xiaochi/guangdong/',
-    'http://www.meishij.net/china-food/xiaochi/beijing/',
-    'http://www.meishij.net/china-food/xiaochi/shanxii/',
-    'http://www.meishij.net/china-food/xiaochi/shandong/',
-    'http://www.meishij.net/china-food/xiaochi/shanxi/',
-    'http://www.meishij.net/china-food/xiaochi/hunan/',
-    'http://www.meishij.net/china-food/xiaochi/henan/',
-    'http://www.meishij.net/china-food/xiaochi/shanghai/',
-    'http://www.meishij.net/china-food/xiaochi/jiangsu/',
-    'http://www.meishij.net/china-food/xiaochi/hubei/',
-    'http://www.meishij.net/china-food/xiaochi/chongqing/',
-    'http://www.meishij.net/china-food/xiaochi/tianjin/',
-    'http://www.meishij.net/china-food/xiaochi/hebei/',
-    'http://www.meishij.net/china-food/xiaochi/zhejiang/',
-    'http://www.meishij.net/china-food/xiaochi/xinjiang/',
-    'http://www.meishij.net/china-food/xiaochi/jiangxi/',
-    'http://www.meishij.net/china-food/xiaochi/fujian/',
-    'http://www.meishij.net/china-food/xiaochi/guangxi/',
-    'http://www.meishij.net/china-food/xiaochi/yunnan/',
-    'http://www.meishij.net/china-food/xiaochi/liaoning/',
-    'http://www.meishij.net/china-food/xiaochi/jilin/',
-    'http://www.meishij.net/china-food/xiaochi/guizhou/',
-    'http://www.meishij.net/china-food/xiaochi/anhui/',
-    'http://www.meishij.net/china-food/xiaochi/taiwan/',
-    'http://www.meishij.net/china-food/xiaochi/gansu/',
-    'http://www.meishij.net/china-food/xiaochi/xianggang/',
-    'http://www.meishij.net/china-food/xiaochi/menggu/',
-    'http://www.meishij.net/china-food/xiaochi/ningxia/',
-    'http://www.meishij.net/china-food/xiaochi/qinghai/',
-    'http://www.meishij.net/china-food/xiaochi/hainan/',
-    'http://www.meishij.net/china-food/xiaochi/xizang/',
-    'http://www.meishij.net/china-food/xiaochi/chengduxiaochi/',
-    'http://www.meishij.net/china-food/xiaochi/heilongjiang/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/hanguo/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/japan/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/ccmd/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/faguo/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/yidali/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/usa/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/dongnanya/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/moxige/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/aozhou/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/other/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/canqianxiaochi/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/tangpin/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/zhucai/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/zhushi/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/yinpin/',
-    'http://www.meishij.net/chufang/diy/guowaicaipu1/tiandian/',
-    'http://www.meishij.net/hongpei/dangaomianbao/',
-    'http://www.meishij.net/hongpei/bingganpeifang/',
-    'http://www.meishij.net/hongpei/tianpindianxin/',
-    'http://www.meishij.net/hongpei/hongpeigongju/',
-    'http://www.meishij.net/hongpei/hongpeichangshi/',
-    'http://www.meishij.net/hongpei/hongpeiyuanliao/',
-    ]
+entries = {
+    'jiangchangcaipu': 'http://www.meishij.net/chufang/diy/jiangchangcaipu/',
+    'sijiacai': 'http://www.meishij.net/chufang/diy/sijiacai/',
+    'langcaipu': 'http://www.meishij.net/chufang/diy/langcaipu/',
+    'haixian': 'http://www.meishij.net/chufang/diy/haixian/',
+    'recaipu': 'http://www.meishij.net/chufang/diy/recaipu/',
+    'tangbaocaipu': 'http://www.meishij.net/chufang/diy/tangbaocaipu/',
+    'sushi': 'http://www.meishij.net/chufang/diy/sushi/',
+    'jiangliaozhanliao': 'http://www.meishij.net/chufang/diy/jiangliaozhanliao/',
+    'weibolucaipu': 'http://www.meishij.net/chufang/diy/weibolucaipu/',
+    'huoguo': 'http://www.meishij.net/chufang/diy/huoguo/',
+    'tianpindianxin': 'http://www.meishij.net/chufang/diy/tianpindianxin/',
+    'gaodianxiaochi': 'http://www.meishij.net/chufang/diy/gaodianxiaochi/',
+    'ganguo': 'http://www.meishij.net/chufang/diy/ganguo/',
+    'rujiangcai': 'http://www.meishij.net/chufang/diy/rujiangcai/',
+    'yinpin': 'http://www.meishij.net/chufang/diy/yinpin/',
+    'zaocan': 'http://www.meishij.net/chufang/diy/zaocan/',
+    'wucan': 'http://www.meishij.net/chufang/diy/wucan/',
+    'wancan': 'http://www.meishij.net/chufang/diy/wancan/',
+    'xiawucha': 'http://www.meishij.net/chufang/diy/xiawucha/',
+    'yexiao': 'http://www.meishij.net/chufang/diy/yexiao/',
+    'laonian': 'http://www.meishij.net/chufang/diy/laonian/',
+    'chanfu': 'http://www.meishij.net/chufang/diy/chanfu/',
+    'yunfu': 'http://www.meishij.net/chufang/diy/yunfu/',
+    'baobaocaipu': 'http://www.meishij.net/chufang/diy/baobaocaipu/',
+    'jibingtiaoli': 'http://www.meishij.net/yaoshanshiliao/jibingtiaoli/',
+    'gongnengxing': 'http://www.meishij.net/yaoshanshiliao/gongnengxing/',
+    'zangfu': 'http://www.meishij.net/yaoshanshiliao/zangfu/',
+    'renqunshanshi': 'http://www.meishij.net/yaoshanshiliao/renqunshanshi/',
+    'chuancai': 'http://www.meishij.net/china-food/caixi/chuancai/',
+    'xiangcai': 'http://www.meishij.net/china-food/caixi/xiangcai/',
+    'yuecai': 'http://www.meishij.net/china-food/caixi/yuecai/',
+    'dongbeicai': 'http://www.meishij.net/china-food/caixi/dongbeicai/',
+    'lucai': 'http://www.meishij.net/china-food/caixi/lucai/',
+    'zhecai': 'http://www.meishij.net/china-food/caixi/zhecai/',
+    'sucai': 'http://www.meishij.net/china-food/caixi/sucai/',
+    'qingzhencai': 'http://www.meishij.net/china-food/caixi/qingzhencai/',
+    'mincai': 'http://www.meishij.net/china-food/caixi/mincai/',
+    'hucai': 'http://www.meishij.net/china-food/caixi/hucai/',
+    'jingcai': 'http://www.meishij.net/china-food/caixi/jingcai/',
+    'hubeicai': 'http://www.meishij.net/china-food/caixi/hubeicai/',
+    'huicai': 'http://www.meishij.net/china-food/caixi/huicai/',
+    'yucai': 'http://www.meishij.net/china-food/caixi/yucai/',
+    'xibeicai': 'http://www.meishij.net/china-food/caixi/xibeicai/',
+    'yuguicai': 'http://www.meishij.net/china-food/caixi/yuguicai/',
+    'jiangxicai': 'http://www.meishij.net/china-food/caixi/jiangxicai/',
+    'shancicai': 'http://www.meishij.net/china-food/caixi/shancicai/',
+    'guangxicai': 'http://www.meishij.net/china-food/caixi/guangxicai/',
+    'gangtai': 'http://www.meishij.net/china-food/caixi/gangtai/',
+    'other': 'http://www.meishij.net/china-food/caixi/other/',
+    'sichuan': 'http://www.meishij.net/china-food/xiaochi/sichuan/',
+    'guangdong': 'http://www.meishij.net/china-food/xiaochi/guangdong/',
+    'beijing': 'http://www.meishij.net/china-food/xiaochi/beijing/',
+    'shanxii': 'http://www.meishij.net/china-food/xiaochi/shanxii/',
+    'shandong': 'http://www.meishij.net/china-food/xiaochi/shandong/',
+    'shanxi': 'http://www.meishij.net/china-food/xiaochi/shanxi/',
+    'hunan': 'http://www.meishij.net/china-food/xiaochi/hunan/',
+    'henan': 'http://www.meishij.net/china-food/xiaochi/henan/',
+    'shanghai': 'http://www.meishij.net/china-food/xiaochi/shanghai/',
+    'jiangsu': 'http://www.meishij.net/china-food/xiaochi/jiangsu/',
+    'hubei': 'http://www.meishij.net/china-food/xiaochi/hubei/',
+    'chongqing': 'http://www.meishij.net/china-food/xiaochi/chongqing/',
+    'tianjin': 'http://www.meishij.net/china-food/xiaochi/tianjin/',
+    'hebei': 'http://www.meishij.net/china-food/xiaochi/hebei/',
+    'zhejiang': 'http://www.meishij.net/china-food/xiaochi/zhejiang/',
+    'xinjiang': 'http://www.meishij.net/china-food/xiaochi/xinjiang/',
+    'jiangxi': 'http://www.meishij.net/china-food/xiaochi/jiangxi/',
+    'fujian': 'http://www.meishij.net/china-food/xiaochi/fujian/',
+    'guangxi': 'http://www.meishij.net/china-food/xiaochi/guangxi/',
+    'yunnan': 'http://www.meishij.net/china-food/xiaochi/yunnan/',
+    'liaoning': 'http://www.meishij.net/china-food/xiaochi/liaoning/',
+    'jilin': 'http://www.meishij.net/china-food/xiaochi/jilin/',
+    'guizhou': 'http://www.meishij.net/china-food/xiaochi/guizhou/',
+    'anhui': 'http://www.meishij.net/china-food/xiaochi/anhui/',
+    'taiwan': 'http://www.meishij.net/china-food/xiaochi/taiwan/',
+    'gansu': 'http://www.meishij.net/china-food/xiaochi/gansu/',
+    'xianggang': 'http://www.meishij.net/china-food/xiaochi/xianggang/',
+    'menggu': 'http://www.meishij.net/china-food/xiaochi/menggu/',
+    'ningxia': 'http://www.meishij.net/china-food/xiaochi/ningxia/',
+    'qinghai': 'http://www.meishij.net/china-food/xiaochi/qinghai/',
+    'hainan': 'http://www.meishij.net/china-food/xiaochi/hainan/',
+    'xizang': 'http://www.meishij.net/china-food/xiaochi/xizang/',
+    'chengduxiaochi': 'http://www.meishij.net/china-food/xiaochi/chengduxiaochi/',
+    'heilongjiang': 'http://www.meishij.net/china-food/xiaochi/heilongjiang/',
+    'hanguo': 'http://www.meishij.net/chufang/diy/guowaicaipu1/hanguo/',
+    'japan': 'http://www.meishij.net/chufang/diy/guowaicaipu1/japan/',
+    'ccmd': 'http://www.meishij.net/chufang/diy/guowaicaipu1/ccmd/',
+    'faguo': 'http://www.meishij.net/chufang/diy/guowaicaipu1/faguo/',
+    'yidali': 'http://www.meishij.net/chufang/diy/guowaicaipu1/yidali/',
+    'usa': 'http://www.meishij.net/chufang/diy/guowaicaipu1/usa/',
+    'dongnanya': 'http://www.meishij.net/chufang/diy/guowaicaipu1/dongnanya/',
+    'moxige': 'http://www.meishij.net/chufang/diy/guowaicaipu1/moxige/',
+    'aozhou': 'http://www.meishij.net/chufang/diy/guowaicaipu1/aozhou/',
+    'other': 'http://www.meishij.net/chufang/diy/guowaicaipu1/other/',
+    'canqianxiaochi': 'http://www.meishij.net/chufang/diy/guowaicaipu1/canqianxiaochi/',
+    'tangpin': 'http://www.meishij.net/chufang/diy/guowaicaipu1/tangpin/',
+    'zhucai': 'http://www.meishij.net/chufang/diy/guowaicaipu1/zhucai/',
+    'zhushi': 'http://www.meishij.net/chufang/diy/guowaicaipu1/zhushi/',
+    'yinpin': 'http://www.meishij.net/chufang/diy/guowaicaipu1/yinpin/',
+    'tiandian': 'http://www.meishij.net/chufang/diy/guowaicaipu1/tiandian/',
+    'dangaomianbao': 'http://www.meishij.net/hongpei/dangaomianbao/',
+    'bingganpeifang': 'http://www.meishij.net/hongpei/bingganpeifang/',
+    'tianpindianxin': 'http://www.meishij.net/hongpei/tianpindianxin/',
+    'hongpeigongju': 'http://www.meishij.net/hongpei/hongpeigongju/',
+    'hongpeichangshi': 'http://www.meishij.net/hongpei/hongpeichangshi/',
+    'hongpeiyuanliao': 'http://www.meishij.net/hongpei/hongpeiyuanliao/',
+}
 
 
-data = {}
+def get(url):
+    logging.info('GET: %s' % url)
+    session = requesocks.session()
+    r = session.get(url)
+    return r.text
 
 
 def grab_dish(link):
-    page = requests.get(link).text
+    page = get(link)
     if page == '404 Not Found':
-        return
+        logging.warning('404: %s' % link)
+        return None, None
     parser = pq(page)
-    dish = {}
+    if len(parser('.notfound404_wraper')) == 1:
+        logging.warning('404: %s' % link)
+        return None, None
     try:
+        dish = {}
         title = parser('.title')[0][0].text
+        dish['title'] = title
+        dish['craft'] = parser('.w127')[0][1].text
+        dish['taste'] = parser('.w127')[1][1].text
+        dish['difficulty'] = parser('.w270')[0][1][2].text
+        dish['people'] = int('0' + parser('.w270')[1][1][2].text[:-2])
+        dish['prepare_time'] = parser('.w270')[2][1][2].text
+        dish['process_time'] = parser('.w270')[3][1][2].text
+        desc_html = parser('.materials p').html()
+        dish['description'] = '' if desc_html is None else desc_html[18:-18]
+        dish['main_ingre'] = {}
+        for i in parser('.zl h4'):
+            dish['main_ingre'][i[0].text] = i[1].text
+        dish['sub_ingre'] = {}
+        for i in parser('.fuliao li'):
+            dish['sub_ingre'][i[0][0].text] = i[1].text
+        logging.info('OK: %s' % link)
+        return title, dish
     except:
-        print 'ERR'
-        print page
-    dish['title'] = title
-    dish['craft'] = parser('.w127')[0][1].text
-    dish['taste'] = parser('.w127')[1][1].text
-    dish['difficulty'] = parser('.w270')[0][1][2].text
-    dish['people'] = int('0' + parser('.w270')[1][1][2].text[:-2])
-    dish['prepare_time'] = parser('.w270')[2][1][2].text
-    dish['process_time'] = parser('.w270')[3][1][2].text
-    dish['description'] = parser('.materials p').html()[18:-18]
-    dish['main_ingre'] = {i[0].text: i[1].text for i in parser('.zl h4')}
-    dish['sub_ingre'] = {i[0][0].text: i[1].text for i in parser('.fuliao li')}
-    data[title] = dish
+        logging.warning('parsing: %s' % link)
+        return None, None
 
 
-def grab_list(entry):
-    dishes = []
+def grab_list(prefix, entry):
+    data = {}
     link = entry
     while link is not None:
-        page = requests.get(entry).text
+        page = get(entry)
         parser = pq(page)
         hrefs = [tile.attrib['href'] for tile in parser('.big')]
-        dishes += [gevent.spawn(grab_dish, href) for href in hrefs]
+        for href in hrefs:
+            title, dish = grab_dish(href)
+            if title is not None:
+                data[title] = dish
+            time.sleep(0.8)
         next_page = parser('.next')
         link = None if len(next_page) == 0 else next_page[0].attrib['href']
-    gevent.joinall(dishes)
-    print 'OK: %s' % entry
+    with open('result/%s.json' % prefix, 'w') as f:
+        f.write(json.dumps([i[1] for i in data.iteritems()]))
+    logging.info('OK: %s' % entry)
 
 
 if __name__ == '__main__':
-    gevent.joinall([gevent.spawn(grab_list, e) for e in entries])
-    with open('result', 'w') as f:
-        f.write(json.dumps([i[1] for i in data.iteritems()]))
+    logging.getLogger().setLevel(logging.INFO)
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+    rootLogger = logging.getLogger()
+    fileHandler = logging.FileHandler("spider.log")
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
+
+    for p, e in entries.iteritems():
+        grab_list(p, e)
     print 'Done.'
